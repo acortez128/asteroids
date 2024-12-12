1b8463201d4f717df66796d455ed993a8e0ce9c8
acortez128 <acortez128@gmail.com>
2024-12-11 23:05:25 -0800

    Created the circleshape.py file to control the displayed objects

--- asteroids/main.py
+++ asteroids/main.py
@@ -6,17 +6,22 @@ print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black") 
+        pygame.display.flip()
 
-pygame.init()
+        dt = clock.tick(60) / 1000
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-black_color = (0,0,0)
-
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill(black_color, rect=None, special_flags=0) 
-    pygame.display.flip()    
+if __name__ == "__main__":
+    main()
